b7c1425ee94ee4049fc58183539ef9868175fde5
monica <monicalpaiva@hotmail.com>
2022-09-24 19:33:59 -0300

    Estudos IMD - estruturada

--- Programação_Estruturada/aula_05/script.js
+++ Programação_Estruturada/aula_05/script.js
@@ -1,17 +1,17 @@
 function situacao() {
   var saida = resultado();
-  document.getElementById('resultado').innerHTML = "O resultado é " + saida;
+  document.getElementById('resultado').innerHTML = "O valor é aprovado: " + saida;
 }
 
 function resultado() {
   var x = Number(document.getElementById('N1').value);
   var y = Number(document.getElementById('N2').value);
-  document.getElementById('depuracao1').innerHTML = x; // informar o valor para depuração
+  document.getElementById('depuracao1').innerHTML = "Valor do primeiro número:" + x; // informar o valor para depuração
 
   var m = media(x,y);
   var r = m >= 7;
 
-  document.getElementById('depuracao2').innerHTML = m;
+  document.getElementById('depuracao2').innerHTML = "Valor do resultado da média" + m;
 
   return r;
 }
