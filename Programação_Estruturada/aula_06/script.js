d0f7d3a649a493e2fd3b71083842d8add9e592b6
monica <monicalpaiva@hotmail.com>
2022-09-25 22:52:42 -0300

    Estudos IMD - estruturada - aula 06

--- Programação_Estruturada/aula_06/script.js
+++ Programação_Estruturada/aula_06/script.js
@@ -1,53 +1,9 @@
-function situacao() {
-  var saida = resultado();
-  document.getElementById('resultado').innerHTML = "O valor é aprovado: " + saida;
-}
+var x = 999999999999999;
+var y = 9999999999999999;
+document.getElementById("texto1").innerHTML = "x = " + x + "<br>" + "y = " + y;
 
-function resultado() {
-  var x = Number(document.getElementById('N1').value);
-  var y = Number(document.getElementById('N2').value);
-  document.getElementById('depuracao1').innerHTML = "Valor do primeiro número:" + x; // informar o valor para depuração
+var z = 0.2 + 0.1;
+document.getElementById("texto2").innerHTML = "0.2 + 0.1 = " + z;
 
-  var m = media(x,y);
-  var r = m >= 7;
-
-  document.getElementById('depuracao2').innerHTML = "Valor do resultado da média" + m;
-
-  return r;
-}
-
-/**
-function media(a, b) {
-  var c = (a + b)/2;
-
-  document.write(c); // A página vai ser limpa, aparecendo apenas o resultado
-  window.alert(x);  // Uma janela de alerta vai aparecer com o resultado
-
-  return c;
-}
-**/
-
-function media(a, b) {
-  var c = (a + b)/2;
-  return c;
-}
-
-/**segunda explicação */
-function condicao() {
-  var saida = resultado01();
-  document.getElementById('resultado01').innerHTML = "O resultado é " + saida;
-}
-
-function resultado01() {
-  var x = Number(document.getElementById('Nota01').value);
-  var y = Number(document.getElementById('Nota02').value);
-  var m = media1(x,y);
-  var r = m >= 7;
-
-  return r;
-}
-
-function media1(a, b) {
-  var c = (a + b)/2;
-  return c;
-}
+var w = (0.2*10 + 0.1*10) / 10;
+document.getElementById("texto3").innerHTML = "0.2 + 0.1 = " + w;
