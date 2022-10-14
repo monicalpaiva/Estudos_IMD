e62f3a81f32efebcb633fd0adb656dac136611a9
monica <monicalpaiva@hotmail.com>
2022-10-14 13:11:45 -0300

    #aula 10

--- Programação_Estruturada/aula_09/script.js
+++ Programação_Estruturada/aula_09/script.js
@@ -69,5 +69,87 @@ if (document.getElementById(nome_elemento)) {
 }
 }
 
+//Curto-Circuito
+
+var texto;
+
+// Curto-circuito nos condicionais
+
+if (true || null.toString()) {
+    texto = "Resultado é true";
+} else {
+    texto = "Resultado é false";
+}
+
+// Curto-circuito nas atribuições
+var texto2;
+texto2 = "Programação Estruturada";
+texto2 = (texto2 || "Programação OO");
+
+texto2 = "";
+texto2 = (texto2 || "Programação OO");
+
+var x = 0;
+var y = 1;
+var z = 2;
+if (x || y || z) {
+    texto2 = "Resultado é true";
+} else {
+    texto2 = "Resultado é false";
+}
+
+document.getElementById("circuito1").innerHTML = texto;
+document.getElementById("circuito2").innerHTML = texto2;
                 
-   
+// Switch
+
+var data = new Date(1969, 07, 20);
+var dia = data.getDay();
+
+document.getElementById("DiaDaSemana").innerHTML = dia_da_semana(dia);
+
+function dia_da_semana(dia) {
+    var resposta = "";
+    switch (dia) {
+        case 0:
+            resposta = "Domingo";
+            break;
+        case 1:
+            resposta = "Segunda-feira";
+            break;
+        case 2:
+            resposta = "Terça-feira";
+            break;
+        case 3:
+            resposta = "Quarta-feira";
+            break;
+        case 4:
+            resposta = "Quinta-feira";
+            break;
+        case 5:
+            resposta = "Sexta-feira";
+            break;
+        case 6:
+            resposta = "Sábado";
+            break;
+        default:
+            resposta = "Dia Inválido";
+    }
+    
+    switch (dia) {
+        case 0:
+        case 6:
+            resposta = resposta + ". Fim de Semana.";
+            break;
+        case 1:
+        case 2:
+        case 3:
+        case 4:
+        case 5:
+            resposta = resposta + ". É um dia Útil.";
+            break;
+    }
+    
+    return resposta;
+}
+                        
--- Programação_Estruturada/aula_10/script.js
+++ Programação_Estruturada/aula_10/script.js
@@ -69,5 +69,80 @@ if (document.getElementById(nome_elemento)) {
 }
 }
 
+// Condição FOR
+
+function contagem(entrada1) {
+    var n = Number(document.getElementById(entrada1).value);
+
+    var texto1 = "";
+    var texto2 = "";
+    var texto3 = "";
+    var texto4 = "";
+    var texto5 = "";
+    var texto6 = "";
+
+    for (i = 1; i <= n; i++) {
+        texto6 = texto6 + i + "<br>";
+    }
+
+
+    // Declarando a variável de controle antes do laço
+
+    var i = 1;
+    for (;i <= n; i++) {
+        texto5 = texto5 + i + "<br>";
+    }
+
+
+    // Declarando mais de uma variável de controle
+
+    for (i = 1, j = n; i <= n; i++, j--) {
+        texto4 = texto4 + i + " - " + j + "<br>";
+    }
+
+
+    // Incrementando a variável de controle dentro do laço
+
+    for (i = 1; i <= n; ) {
+        texto3 = texto3 + i + "<br>";
+        i++;
+    }
+
+
+    // Removendo a condição
+
+    for (i = 1; ; i++) {
+        texto2 = texto2 + i + "<br>";
+        if (i >= n) {
+            break;
+        }
+    }
+
+
+    // Utilizando o continue
+    for (i = 1; i <= n; i++) {
+        if (i == 3) {
+            continue;
+        }
+        texto1 = texto1 + i + "<br>";
+    }
+
+    document.getElementById("resultadofor1").innerHTML = texto1;
+    document.getElementById("resultadofor2").innerHTML = texto2;
+    document.getElementById("resultadofor3").innerHTML = texto3;
+    document.getElementById("resultadofor4").innerHTML = texto4;
+    document.getElementById("resultadofor5").innerHTML = texto5;
+    document.getElementById("resultadofor6").innerHTML = texto6;
+}
                 
-   
+   // Laço_For_Of
+
+   function quebraLinhas(entrada3) {
+    var str = document.getElementById(entrada3).value;
+
+    var texto7 = "Letras de "+str+":<br>";
+    for (c of str) {
+        texto7 = texto7 + c + "<br>";
+    }
+    document.getElementById("resultadofor7").innerHTML = texto7;
+}
