2e47eba1396a8d95728654c3886a86d2df084e9d
monica <monicalpaiva@hotmail.com>
2022-10-29 23:41:29 -0300

    #Programação estruturada

--- Programação_Estruturada/script/script_aula_11.js
+++ Programação_Estruturada/script/script_aula_11.js
@@ -31,7 +31,19 @@ function dia_da_semana2() {
     texto1 = "Tamanho = "+ tamanho + "<br>";
     texto1 = texto1 + "Primeiro = " + primeiro + "<br>";
     texto1 = texto1 + "Ultimo = " + ultimo;
-    
+    dias[2]= "TUESDAY";    
 
     document.getElementById("resultado2").innerHTML = texto1 + "<br>" + dias;
+}
+
+// Preenchendo os arrays
+
+function nomes(){
+    var pessoas = [];
+    var total = Number(document.getElementById("total").value);
+    for (i=0; i<total;i++){
+        var pessoa = prompt("Entre com a "+ i + " pessoa");
+        pessoas[i]  = pessoa;
+    }
+    document.getElementById("resultado3").innerHTML = pessoas;
 }
